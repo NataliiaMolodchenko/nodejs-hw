70c681c16921cb5c5ee1992b25f8296a82bdeed6
NataliiaMolodchenko <natalie.molodchenko@gmail.com>
2026-03-13 17:33:04 +0200

    fixed after check

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -5,13 +5,11 @@ export const upload = multer({
   limits: {
     fileSize: 2 * 1024 * 1024,
   },
-  fileFilter: (req, file, cb) => {
-	  const allowedTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif', 'image/webp'];
-
-	  if (allowedTypes.includes(file.mimetype)) {
+	  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith('image/')) {
       cb(null, true);
-	  } else {
-	    cb(new Error('Only images allowed'), false);
-	  }
+    } else {
+      cb(new Error('Only images allowed'), false);
+    }
   },
 });
