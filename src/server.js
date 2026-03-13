dffff988870d579428030990f64e5213c9f7dc15
NataliiaMolodchenko <natalie.molodchenko@gmail.com>
2026-03-13 17:17:38 +0200

    add tasks

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 import { errors } from "celebrate";
 import authRoutes from './routes/authRoutes.js';
 import cookieParser from 'cookie-parser';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 app.use(authRoutes);
+app.use(userRoutes);
 app.use(notesRoutes);
 app.use(notFoundHandler);
 app.use(errors());
